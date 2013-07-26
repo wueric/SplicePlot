4c4160c2f0663091a120752008e66e8bc8039abe
Eric Wu <wu.eric.g@gmail.com>
2013-07-26 10:45:47 -0700

    Added support for writing plots to user-specified directories

--- plot.py
+++ plot.py
@@ -13,6 +13,7 @@ if __name__ == '__main__':
 
     parser.add_argument('pickle',type=str,help='Location of pickle file for plotting')
     parser.add_argument('settings',type=str,help='Location of the settings file')
+    parser.add_argument('--output',type=str,default=None,help='Location of output folder. Optional argument')
 
     args = parser.parse_args()
 
@@ -21,6 +22,19 @@ if __name__ == '__main__':
 
 
     try:
+
+        # if an output directory is specified, check if it exists and create it if it doesn't
+        if args.output is not None:
+            try:
+                os.makedirs(args.output)
+            except:
+                if os.path.isdir(args.output):
+                    pass
+                else:
+                    print 'Could not create directory {0}'.format(args.output)
+                    raise Exception
+
+
         # extract data from pickle file
         pickle_file = open(args.pickle,'rb')
         varpos = pickle.load(pickle_file)
@@ -35,23 +49,28 @@ if __name__ == '__main__':
         plot_name_stem = args.pickle.split('/')
         plot_name_stem = plot_name_stem[len(plot_name_stem)-1]
 
+
+        file_write_stem = '{0}/plots'.format(os.path.dirname(os.path.abspath(__file__)))
+        if args.output is not None:
+            file_write_stem = args.output.strip('/')
+
         if hive_plot_settings['draw_hive_plot']:
             print 'Drawing hive plot...'
-            draw_hive_plot(file_name='{0}/plots/{1}_hive.svg'.format(os.path.dirname(os.path.abspath(__file__)),plot_name_stem),
+            draw_hive_plot(file_name='{0}/{1}_hive.svg'.format(file_write_stem,plot_name_stem),
                     data=data_frame,
                     hive_plot_settings=hive_plot_settings,
                     genotype_ordering=ordered_genotypes_list)
 
         if struct_plot_settings['draw_struct_plot']:
             print 'Drawing structure plot...'
-            draw_population_structure_graph(output_file_name='{0}/plots/{1}_structure.svg'.format(os.path.dirname(os.path.abspath(__file__)),plot_name_stem),
+            draw_population_structure_graph(output_file_name='{0}/{1}_structure.svg'.format(file_write_stem,plot_name_stem),
                                         data=data_frame,
                                         genotypes_ordering=ordered_genotypes_list,
                                         struct_plot_settings=struct_plot_settings)
 
         if sashimi_plot_settings['draw_sashimi_plot']:
             print 'Drawing sashimi plot...'
-            draw_sashimi_plot(output_file_path='{0}/plots/{1}_sashimi.svg'.format(os.path.dirname(os.path.abspath(__file__)),plot_name_stem),
+            draw_sashimi_plot(output_file_path='{0}/{1}_sashimi.svg'.format(file_write_stem,plot_name_stem),
                         settings=sashimi_plot_settings,
                         var_pos=varpos,
                         average_depths_dict=genotype_averages_dict,
